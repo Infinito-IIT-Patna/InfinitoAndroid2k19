77afb1665f3e11e21215a4fddb545c957106a112
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-01-18 10:59:15 +0530

    remove errors

--- app/src/androidTest/java/com/mind/INFINITO/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/mind/INFINITO/ExampleInstrumentedTest.java
@@ -1,13 +1,15 @@
 package com.mind.INFINITO;
 
+import android.app.Instrumentation;
 import android.content.Context;
+
 import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,6 +18,8 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    private Instrumentation InstrumentationRegistry;
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
